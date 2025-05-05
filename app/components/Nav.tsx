b9788177f0af8f4ba4a2efba58c9efa77decd6dd
Temidayo Victor <temidayovictor1@gmail.com>
2025-05-05 15:46:17 +0100

    Navigation

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,9 +1,55 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Nav = () => {
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className="header">
+            <div className="flex items-center gap-4">
+                <div>
+                    <Image
+                        aria-hidden
+                        src="/assets/images/logo-1.png"
+                        alt="Colearn Logo"
+                        width={150}
+                        height={54}
+                    />
+                </div>
+
+                <div className="flex items-center justify-between gap-4 text-white mt-2">
+                    <Link href='/' className="nav-link">Explore</Link>
+                    <Link href='/' className="nav-link">About Us</Link>
+                    <Link href='/' className="nav-link">Blog</Link>
+                    <Link href='/' className="nav-link">Become a Tutor</Link>
+                    <Link href='/' className="nav-link">Contact Us</Link>
+                </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+                <div>
+                    <Image
+                        aria-hidden
+                        src="/assets/images/shopping-cart.png"
+                        alt="Colearn Logo"
+                        width={40}
+                        height={40}
+                        className="object-contain"
+                    />
+                </div>
+                <div>
+                    <Link href='/' className="flex items-center gap-2 btn text-white">
+                        <Image
+                            aria-hidden
+                            src="/assets/images/profile.png"
+                            alt="Colearn Logo"
+                            width={20}
+                            height={20}
+                        />
+                        <p className="text-[1rem]">Login</p>
+                    </Link>
+                </div>
+            </div>
+
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,70 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+      <div className="cover bg-dark ">
+        <div className="text-white mt-[2rem] flex flex-col items-center justify-center gap-4 text-center">
+          <div>
+            <h2 className="font-semibold text-[2rem]">Learn Without <span className="color-normal">Boundaries</span> to reach your <br /> goals faster.</h2>
+          </div>
+          <div>
+            <p>Discover a vast library of courses, expert mentors, and instructors who will help <br /> you reach your goals. Unlock your full potential and learn without limits!</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Link href='/' className="flex gap-2 btn btn-primary-fill">
+              <p>Join for Free</p>
+              <Image
+                aria-hidden
+                src="/assets/images/arrow-right.png"
+                alt="Colearn Logo"
+                width={14}
+                height={13}
+                className="object-contain"
+              />
+            </Link>
+            <Link href='/' className="flex gap-2 btn">
+              <Image
+                aria-hidden
+                src="/assets/images/book-saved.png"
+                alt="Colearn Logo"
+                width={20}
+                height={20}
+                className="object-contain"
+              />
+              <p>Explore</p>
+            </Link>
+          </div>
+
+          <div className="">
+            <div className="flex items-center justify-between bg-white gradient-border w-[40rem] my-[3em] py-[.5em] px-[1.5em] rounded-[.5rem]">
+              <div className="flex items-center gap-2">
+                <Image
+                  aria-hidden
+                  src="/assets/images/search-normal.png"
+                  alt="Colearn Logo"
+                  width={20}
+                  height={20}
+                  className="object-contain"
+                />
+                <input type="text" placeholder="Search for courses, instructors, or topics..." className="search-input w-[100%] placeholder-gray-400" />
+              </div>
+              <div>
+                <Link href='/' className="flex gap-2 btn btn-primary-fill">
+                  <Image
+                    aria-hidden
+                    src="/assets/images/search-light.png"
+                    alt="Colearn Logo"
+                    width={20}
+                    height={20}
+                    className="object-contain"
+                  />
+                  <p>Search</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
