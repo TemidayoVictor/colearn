09a838c9fd0e6490954e8896b4c7bcba5a3626fe
Temidayo Victor <temidayovictor1@gmail.com>
2025-05-06 11:22:20 +0100

    Home

--- app/components/HomeCourseList.tsx
+++ app/components/HomeCourseList.tsx
@@ -6,32 +6,39 @@ import 'swiper/css';
 import 'swiper/css/autoplay';
 import { Swiper, SwiperSlide } from 'swiper/react'; 
 import { Autoplay } from 'swiper/modules';
+import { motion } from 'framer-motion';
 const images = ["/assets/images/image-2.png", "/assets/images/image-3.png", "/assets/images/image-4.png"];
 
 const HomeCourseList = () => {
     return (
         <div className="mt-[2em]">
-            <Swiper
-                modules={[Autoplay]}
-                slidesPerView="auto"
-                spaceBetween={20}
-                loop={true}
-                speed={5000}
-                autoplay={{
-                  delay: 1,
-                  disableOnInteraction: false,
-                }}
-                grabCursor={true}
-                className="w-full overflow-hidden"
-            >
-                {
-                    images.map((src, index) => (
-                        <SwiperSlide key={index} style={{ width: '200px' }}>
-                            <Image src={src} alt={`image-${index}`} width={200} height={200} />
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+            <div className="flex flex-col gap-8">
+            
+                <div className="overflow-hidden w-full">
+                    <motion.div
+                        className="flex gap-4 w-max"
+                        animate={{ x: ['100%', '-100%'] }}
+                        transition={{
+                            repeat: Infinity,
+                            ease: 'linear',
+                            duration: 20, // Adjust this for speed
+                        }}
+                    >
+                        {
+                            [...images, ...images].map((src, index) => (
+                                <Image
+                                key={index}
+                                src={src}
+                                alt={`Scrolling image ${index}`}
+                                width={200}
+                                height={200}
+                                className="rounded-lg object-cover"
+                                />
+                            ))
+                        }
+                    </motion.div>
+                </div>
+            </div>
         </div>
     )
 }
