72c6f547e6d981513a7d2bd662e0dd97aee5200c
Temidayo Victor <temidayovictor1@gmail.com>
2025-05-21 11:38:35 +0100

    instructors courses

--- app/components/Instructors/DashboardTopCoursesTable.tsx
+++ app/components/Instructors/DashboardTopCoursesTable.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import TableContent from "./TableContent";
 
 const DashboardTopCoursesTable = () => {
     return (
@@ -18,51 +19,8 @@ const DashboardTopCoursesTable = () => {
                     />
                 </div>
             </div>
-            <div className="table-container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>S/N</th>
-                            <th>Product</th>
-                            <th>Total Revenue</th>
-                            <th>Total  Enrollment</th>
-                            <th>Total Completion</th>
-                            <th>Review</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            [1,2,3,4,5,6].map((items, index) => (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td className="flex items-center gap-2">
-                                        <Image
-                                            aria-hidden
-                                            src="/assets/images/top-course.png"
-                                            alt="Colearn Logo"
-                                            width={40}
-                                            height={40}
-                                            className="object-contain"
-                                        />
-
-                                        <span className="flex flex-col overflow-hidden">
-                                        <span className="text-sm font-semibold text-gray-800 truncate sm:whitespace-normal sm:truncate-0">
-                                            Data Science and. . . 
-                                        </span>
-                                        <span className="text-xs text-gray-500 mt-1 line-clamp-2">
-                                            Data Science and. . .
-                                        </span>
-                                        </span>
-                                    </td>
-                                    <td>$4000</td>
-                                    <td>23</td>
-                                    <td>23</td>
-                                    <td>20</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+            <div className="mt-[1.5em]">
+                <TableContent />
             </div>
         </div>
     )
---
+++ app/components/Instructors/TableContent.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Image from "next/image";
+
+const TableContent = () => {
+    return (
+        <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>S/N</th>
+                        <th>Product</th>
+                        <th>Total Revenue</th>
+                        <th>Total  Enrollment</th>
+                        <th>Total Completion</th>
+                        <th>Review</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        [1,2,3,4,5,6].map((items, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td className="flex items-center gap-2">
+                                    <Image
+                                        aria-hidden
+                                        src="/assets/images/top-course.png"
+                                        alt="Colearn Logo"
+                                        width={40}
+                                        height={40}
+                                        className="object-contain"
+                                    />
+
+                                    <span className="flex flex-col overflow-hidden">
+                                    <span className="text-sm font-semibold text-gray-800 truncate sm:whitespace-normal sm:truncate-0">
+                                        Data Science and. . . 
+                                    </span>
+                                    <span className="text-xs text-gray-500 mt-1 line-clamp-2">
+                                        Data Science and. . .
+                                    </span>
+                                    </span>
+                                </td>
+                                <td>$4000</td>
+                                <td>23</td>
+                                <td>23</td>
+                                <td>20</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default TableContent;
