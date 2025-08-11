0b258fe497a55f28c7a7ea676da7f4ed0675154f
Temidayo Victor <temidayovictor1@gmail.com>
2025-08-11 04:24:04 +0100

    clean ups

--- app/students/bookings/page.tsx
+++ app/students/bookings/page.tsx
@@ -2,13 +2,16 @@
 import React from "react";
 import UserLayout from "@/app/components/UserLayout";
 import StudentBookingBody from "@/app/components/Students/StudentBookingBody";
+import { Suspense } from "react";
 
 const StudentBooking = () => {
     return (
         <div>
             <UserLayout userType="student">
                 <div className="container-3">
-                    <StudentBookingBody userType = "student" />
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <StudentBookingBody userType = "student" />
+                    </Suspense>
                 </div>
             </UserLayout>
         </div>
