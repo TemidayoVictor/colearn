ea705bbe794fd6c67a8c84f9f5f2ce5edb554956
Temidayo Victor <temidayovictor1@gmail.com>
2025-08-28 23:26:50 +0100

    clean ups

--- app/components/Admin/AdminManagementBody.tsx
+++ app/components/Admin/AdminManagementBody.tsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 import DashboardPerformance from "../Instructors/DashboardPerformance";
 import AdminVerificationTable from "./AdminVerificationTable";
 import { all_users_admin } from "@/services/admin";
+import { admin_dashboard } from "@/services/admin";
 import { useAuthAdmin } from "@/hooks/useAuth";
 import { genralStore } from "@/zustand/generalStore";
 import { useRouter } from "next/navigation";
@@ -42,6 +43,34 @@ const AdminManagementBody = () => {
         };
         init();
 
+    }, []);
+
+    // fetch data for users management dashboard
+
+    useEffect(() => {
+        const init = async () => {
+            await useAuthAdmin(router);
+            try {
+                const response = await admin_dashboard();
+                
+                if (response.success) {
+                    // save state globally
+                    genralStore.getState().setData(response.data);
+                } 
+    
+                else {
+                    showErrorToast(response.message)
+                    console.log(response)
+                }
+            }
+
+            catch(error: any) {
+                showErrorToast('Something unexpected happened')
+                console.log(error)
+            }
+            setLoading(false);
+        };
+        init();
     }, []);
 
     if (loading) return <Loader />
